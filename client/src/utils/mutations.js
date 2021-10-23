6e3dbe70a02a5620b2365d6fafa503003b724e6d
DN-Vanguard <Domiltannguyen@outlook.com>
2021-10-23 08:58:03 -0700

    updated clients mutation and queries.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,83 +1,74 @@
-import { gql } from '@apollo/client';
+import {gql} from "@apollo/client"
 
-export const LOGIN_USER = gql `
-	mutation login($email: String!, $password: String!) {
-		login(email: $email, password: $password) {
-			token
-			user {
-				_id
-				username
-				email
-				bookCount
-				savedBooks {
-					authors
-					description
-					bookId
-					image
-					link
-					title
-				}
-			}
-		}
-	}
+export const LOGIN_USER = gql`
+mutation login($email:String!, $password:String!) {
+  login(email:$email, password:$password) {
+    token
+    user{
+      username
+      email
+      savedBooks {
+        authors
+        bookId
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+}
 `;
 
-export const ADD_USER = gql `
-	mutation addUser($username: String!, $email: String!, $password: String!) {
-		addUser(username: $username, email: $email, password: $password) {
-			token
-			user {
-				_id
-				username
-				email
-				bookCount
-				savedBooks {
-					authors
-					description
-					bookId
-					image
-					link
-					title
-				}
-			}
-		}
-	}
+export const ADD_USER = gql`
+mutation addUser($username:String!, $email:String!, $password:String!) {
+  addUser(username:$username, email:$email, password:$password) {
+     token
+    user{
+      username
+      email
+      savedBooks {
+        authors
+        bookId
+        description
+        title
+        image
+        link
+      }
+      }
+  }
+}
 `;
 
-export const SAVE_BOOK = gql `
-	mutation saveBook($book: BookInput) {
-		saveBook(book: $book) {
-			_id
-			username
-			email
-			bookCount
-			savedBooks {
-				authors
-				description
-				bookId
-				image
-				link
-				title
-			}
-		}
-	}
+export const SAVE_BOOK = gql`
+	mutation saveBook ($authors:[String!], $description:String!, $title:String!, $bookId:String!, $image:String!, $link:String!) {
+		saveBook(authors:$authors, description:$description, title:$title, bookId:$bookId, image:$image, link:$link) {
+      username
+      email
+      savedBooks {
+        authors
+        bookId
+        description
+        title
+        image
+        link
+      }
+    }
+    }
 `;
 
-export const REMOVE_BOOK = gql `
-	mutation removeBook($bookId: String!) {
-		removeBook(bookId: $bookId) {
-			_id
-			username
-			email
-			bookCount
-			savedBooks {
-				authors
-				description
-				bookId
-				image
-				link
-				title
-			}
-		}
-	}
+export const REMOVE_BOOK = gql`
+	mutation removeBook($bookId:String!) {
+		removeBook(bookId:$bookId) {
+      username
+      email
+      savedBooks {
+        authors
+        bookId
+        description
+        title
+        image
+        link
+      }
+}}
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,20 +1,19 @@
-import { gql } from '@apollo/client';
+//This will hold the query `GET_ME`, which will execute the `me` query set up using Apollo Server.
+import {gql} from "@apollo/client"
 
 export const GET_ME = gql`
-	query me {
-		me {
-			_id
-			username
-			email
-			bookCount
-			savedBooks {
-				authors
-				description
-				bookId
-				image
-				link
-				title
-			}
-		}
-	}
+query {
+  me{
+      username
+      email
+      savedBooks {
+        authors
+        bookId
+        description
+        title
+        image
+        link
+      }
+  }
+}
 `;
