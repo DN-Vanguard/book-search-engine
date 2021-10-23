49ae0f518f2bad9a59ad450783eaa91d75d496a7
DN-Vanguard <Domiltannguyen@outlook.com>
2021-10-23 08:48:05 -0700

    restructuring what to pull from API.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,43 +1,34 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-	type User {
-		_id: ID!
-		username: String!
-		email: String!
-		bookCount: Int
-		savedBooks: [Book]
-	}
-	input BookInput {
-		bookId: String!
-		authors: [String]
-		description: String
-		title: String!
-		image: String
-		link: String
-	}
-	type Book {
-		bookId: String!
-		authors: [String]
-		description: String
-		title: String!
-		image: String
-		link: String
-	}
-	type Auth {
-		token: ID!
-		user: User
-	}
-	type Query {
-		users: [User]
-		me: User
-	}
-	type Mutation {
-		addUser(username: String!, email: String!, password: String!): Auth
-		login(email: String!, password: String!): Auth
-		saveBook(book: BookInput): User
-		removeBook(bookId: String!): User
-	}
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    bookCount: String!
+    savedBooks: [Book!]
+  }
+  type Book {
+    bookId: String!
+    authors: [String!]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+  type Auth {
+    token: String!
+    user: User
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+   login(email:String!, password:String!): Auth
+   addUser(username:String!, email:String!, password:String!): Auth
+   saveBook(authors:[String!], description:String!, title:String!, bookId:String!, image:String!, link:String!): User
+   removeBook(bookId:String!): User
+  }
 `;
 
 module.exports = typeDefs;
